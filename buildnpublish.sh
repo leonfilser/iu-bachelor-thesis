8375c8e988a0e23e91ef64b27067f66e2bced2b6
Leon Filser <leon.filser@gmail.com>
2026-01-10 11:03:21 +0100

    added frontend build instructions

--- buildnpublish.sh
+++ buildnpublish.sh
@@ -20,3 +20,21 @@ else
     echo "Backend build failed" >&2
     exit 1
 fi
+
+# Building the frontend Docker image
+echo "Building frontend..."
+if docker build -t leonfilser/iu-bachelor-thesis-frontend:latest ${PWD}/frontend/; then
+    echo "Frontend image built successfully"
+    echo "Pushing frontend image to registry..."
+
+    if docker push leonfilser/iu-bachelor-thesis-frontend:latest; then
+        echo "Frontend image pushed successfully"
+    else
+        echo "Failed to push frontend image" >&2
+        exit 1
+    fi
+
+else
+    echo "Frontend build failed" >&2
+    exit 1
+fi
